ba8ff577e0b36dccd9a59650ef0092868aeea783
Neesa-Saud <nishasaud2061@gmail.com>
2025-03-22 22:22:19 +0545

    changes made

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,15 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class LOgInPage extends StatefulWidget {
+  const LOgInPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<LOgInPage> createState() => _LOgInPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LOgInPageState extends State<LOgInPage> {
+  final _formkey = GlobalKey<FormState>();
+  bool isLogin = false;
+  String email = '';
+  String password = '';
+  String username = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Email/Pass Auth'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.leave_bags_at_home),
+          ),
+        ],
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        key: _formkey,
+        child: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              !isLogin
+                  ? TextFormField(
+                    key: ValueKey('username'),
+                    decoration: InputDecoration(hintText: "Enter Username"),
+                    validator: (value) {
+                      if (value.toString().length < 3) {
+                        return 'Username is so small';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      setState(() {
+                        username = value!;
+                      });
+                    },
+                  )
+                  : Container(),
+              TextFormField(
+                key: ValueKey('email'),
+                decoration: InputDecoration(hintText: "Enter Email"),
+                validator: (value) {
+                  if (!(value.toString().contains('@'))) {
+                    return 'Invalid Email';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    email = value!;
+                  });
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                key: ValueKey('password'),
+                decoration: InputDecoration(hintText: "Enter Password"),
+                validator: (value) {
+                  if (value.toString().length < 6) {
+                    return 'Password is so small';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    password = value!;
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      _formkey.currentState!.save();
+                      isLogin
+                          ? signin(email, password)
+                          : signup(email, password);
+                    }
+                  },
+                  child: isLogin ? Text('Login') : Text('Signup'),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    isLogin = !isLogin;
+                  });
+                },
+                child:
+                    isLogin
+                        ? Text("Don't have an account? Signup")
+                        : Text('Already Signed Up? Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
